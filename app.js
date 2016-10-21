0f27cbed0f0d8a4d392dff4ed187e69c5075a9d7
Rafael Pacas @rapala61 <rapala61@gmail.com>
2016-10-21 02:43:20 -0400

    implemented disconnection for consumer

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const dotenv = require('dotenv').config({ silent: true });
 
+var CronJob = require('cron').CronJob;
+
 const Consumer = require('./lib/consumer');
 
 const EmailHelper = require('./lib/emailHelper');
 
 const emailHelper = new EmailHelper();
 
-const consumer = new Consumer();
+const job = new CronJob('*/10 * * * * *', () => {
+  const consumer = new Consumer();
+  consumer.consumeMessage('sign_up', (msg) => {
+    const msgBody = JSON.parse(msg.content.toString());
+    const emailBody = `
+    <h2>The following user has signed up</h2>
+    <hr>
+    <strong>name:</strong> ${msgBody.name} <br>
+    <strong>email:</strong> ${msgBody.email}<br>
+    <strong>date of birth:</strong> ${msgBody.dob}<br>
+    <strong>password:</strong> ${msgBody.password}<br>
+    <strong>password confirmation:</strong> ${msgBody.password_confirm}<br>`;
 
-consumer.consumeMessage('sign_up', (msg) => {
-  const msgBody = JSON.parse(msg.content.toString());
-  const emailBody = `
-  <h2>The following user has signed up</h2>
-  <hr>
-  <strong>name:</strong> ${msgBody.name} <br>
-  <strong>email:</strong> ${msgBody.email}<br>
-  <strong>date of birth:</strong> ${msgBody.dob}<br>
-  <strong>password:</strong> ${msgBody.password}<br>
-  <strong>password confirmation:</strong> ${msgBody.password_confirm}<br>`;
-
-  emailHelper.sendEmail('Rafa', 'rapala61@gmail.com', 'signup form submission', emailBody);
-});
+    emailHelper.sendEmail('Rafa', 'rapala61@gmail.com', 'signup form submission', emailBody);
+  });
+}, null, true, 'America/New_York');
